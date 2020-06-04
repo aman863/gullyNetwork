df74a179d9d5699d7f05d38283cd4d34340ad01c
Aman jain <aman29j2001@gmail.com>
2020-06-04 13:42:32 +0530

    Handling errors

--- app.js
+++ app.js
@@ -6,6 +6,8 @@ const globalErrorHandler = require("./controllers/errorControllers");
 //USING MIDDLEWARES
 app.use(express.json());
 
+
+console.log(process.env.NODE_ENV);
 //MOUNTING THE ROUTER
 app.use("/", transactionRouter);
 
--- controllers/transactionControllers.js
+++ controllers/transactionControllers.js
@@ -1,5 +1,6 @@
 const Transaction = require("./../models/transactionModel");
 const catchAsync = require("./../utils/catchAsync");
+const AppError = require("./../utils/appError");
 
 //POST A TRANSACTION
 exports.postTransaction = catchAsync(async (req, res, next) => {
@@ -13,9 +14,38 @@ exports.postTransaction = catchAsync(async (req, res, next) => {
 
 //GET FILTERED TRANSACTIONS
 exports.getTransactions = catchAsync(async (req, res, next) => {
-  let transactions = await Transaction.find(req.query)
+  let queryObj = { ...req.query };
+  let queryString = JSON.stringify(queryObj);
+  //Filtering the transactions
+  const excludedFields = ["page", "limit", "sort", "fields"];
+  excludedFields.forEach((el) => delete queryObj[el]);
+  let query = JSON.stringify(queryObj);
+  queryString = queryString.replace(
+    /\b(gt|gte|lt|lte)\b/g,
+    (match) => `$${match}`
+  );
+  query = JSON.parse(query);
+  //Sorting transactions
+  if (req.query.sort) {
+    sortQuery = req.query.sort;
+  } else {
+    sortQuery = "time";
+  }
+
+  //Paginating transactions
+  const page = parseInt(req.query.page) || 1; //defining default values here
+  const limit = parseInt(req.query.limit) || 100;
+  const skip = (page - 1) * limit;
+  console.log(query);
+  const transactions = await Transaction.find(query)
+    .sort(sortQuery)
+    .skip(skip)
+    .limit(limit)
     .select("-_id")
     .select("-__v");
+  if (transactions.length === 0) {
+    return next(new AppError("No transactions available", 404));
+  }
   res.status(200).json({
     status: "success",
     results: transactions.length,
